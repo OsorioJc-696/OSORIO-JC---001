c39e008124c66a15d90453703811993b06447142
OsorioJC <osoriokalef@gmail.com>
2025-05-29 03:10:42 -0500

    reactualuzar

--- ecommerce-app/frontend/src/store/store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../slice/authSlice.js'; // Asegúrate de importar el slice correcto
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer, // Agrega el authSlice al store
-  },
-});
